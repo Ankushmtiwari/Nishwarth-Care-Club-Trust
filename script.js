5953e9df939640adfb06ad8e9f24f3d0e80c9b23
ankushmtiwari <@ankushg92018@gmail.com>
2025-04-08 17:46:00 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch(form.action, {
+            method: 'POST',
+            body: new FormData(form)
+        })
+        .then(response => {
+            if (response.ok) {
+                window.location.href = 'thankyou.html';
+            } else {
+                alert('Something went wrong. Please try again!');
+            }
+        })
+        .catch(error => {
+            console.error('Error!', error.message);
+            alert('Form submission failed. Please try again.');
+        });
+});
